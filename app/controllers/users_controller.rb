5942d882c3bc467bc3fb0e6e674a24550d5a8770
katsue-watanabe <k-losmama@docomo.ne.jp>
2022-08-15 06:20:17 +0000

    Finish signup-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  def new
+    @user = User.new # ユーザーオブジェクトを生成し、インスタンス変数に代入します。
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = '新規ご登録、誠に有難う存じ上げます。'
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
